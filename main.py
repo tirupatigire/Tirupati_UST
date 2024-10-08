6b8f21caa63eca5c90d424e72f51dabeea346eb1
Gire <tirupatix.gire@intel.com>
2024-10-09 00:50:26 +0530

    modified code main py file to run function parallely

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import threading
+
 from program1 import check_frequency_of_words
 from program2 import check_the_validity_of_password
 
 
 def main():
-    check_frequency_of_words(input_str="which is better python 2 or python 3")
-    check_the_validity_of_password(input_str="asqwr1234@1,aF145#,2w3E*,2We3345")
+    thread1 = threading.Thread(target=check_frequency_of_words, args="which is better python 2 or python 3",)
+    thread2 = threading.Thread(target=check_the_validity_of_password, args="asqwr1234@1,aF145#,2w3E*,2We3345")
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
 
 
 if __name__ == "__main__":
